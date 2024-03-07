ef32678047dc0822d924e2aa6ac9f879f455ab2e
nurik1347 <nurikmirzaev1347@gmail.com>
2024-03-07 16:54:30 +0500

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+let Number1 = prompt('Ism')
+let Number2 = prompt('Familiya')
+let Number3 = prompt('Yosh')
+let Number4 = prompt('Yashash joyi')
+let Number5 = prompt('Yoqtirgan fani')
+
+alert(Number1 + Number2 + Number3 + Number4 + Number5)
+console.log(`Ism :${Number1} \n Familiya :${Number2} \n Yosh:${Number3} \n Yashash joyi:${Number4} \n Yoqtirgan fani :${Number5} \n  `);
+
+
+
